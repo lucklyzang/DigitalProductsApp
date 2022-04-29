e274f6d3a8722df18d1a1388d1e586cd7f3db691
lucklyYong <luckyzangvip@163.com>
2022-04-29 21:30:46 +0800

    删除首页多余代码

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import Vant from 'vant';
 import 'vant/lib/index.css';
 import moment from 'moment'
 import { setStore, getStore, IsPC } from '@/common/js/utils.js'
-import echarts from 'echarts'
+import echarts, { init } from 'echarts'
 if (process.env.NODE_ENV == 'development') {
     Vue.config.devtools = true;
 } else {
@@ -53,4 +53,4 @@ new Vue({
     components: { App },
     created() {},
     template: '<App/>'
-})
+});
